9957242acd299e51a5d2b500c51675dd03027384
Kit <kittsville@googlemail.com>
2021-08-08 13:36:49 +0100

    Add hex UI test

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,23 @@
+const requestFullscreenEl = document.getElementById('request-fullscreen');
+const fullscreenEl = document.getElementById('fullscreen');
+
+requestFullscreenEl.addEventListener('click', ev => fullscreenEl.requestFullscreen());
+
+fullscreenEl.addEventListener('fullscreenchange', ev => {
+  if (document.fullscreenElement) {
+    fullscreenEl.classList.add('active');
+  } else {
+    fullscreenEl.classList.remove('active');
+  }
+})
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+if (params.hasOwnProperty('debug')) {
+  fullscreenEl.classList.add('debug');
+}
+
 // Initialises Material Design Components
 // See: https://github.com/material-components/material-components-web#javascript
 Array.from(document.getElementsByClassName('mdc-text-field')).forEach(mdc.textField.MDCTextField.attachTo);
