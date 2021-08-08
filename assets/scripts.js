401ac16a3e92b7b4961700a3b3672b9fd94ed594
Kit <kittsville@googlemail.com>
2021-08-08 14:23:29 +0100

    Add custom grid sizing

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,7 @@
 const requestFullscreenEl = document.getElementById('request-fullscreen');
 const fullscreenEl = document.getElementById('fullscreen');
+const stylesEl = document.getElementById('dynamic');
+const hexDisplayEl = document.getElementById('hex-display');
 
 requestFullscreenEl.addEventListener('click', ev => fullscreenEl.requestFullscreen());
 
@@ -11,12 +13,43 @@ fullscreenEl.addEventListener('fullscreenchange', ev => {
   }
 })
 
+const generateHexGrid = (width, height) => {
+  const stylesEl = document.createElement('style');
+  stylesEl.type = 'text/css';
+  stylesEl.innerHTML = `
+    #board {
+      width: calc(37px*${width});
+    }
+  `;
+  document.head.appendChild(stylesEl);
+
+  for (let row = 0; row < height; row++) {
+    const rowEl = document.createElement('div');
+    rowEl.classList.add('hex-row', '-clearfix');
+
+    for (let column = 0; column < width; column++) {
+      const hexEl = document.createElement('div');
+      hexEl.classList.add('hex', '-blink');
+
+      rowEl.appendChild(hexEl);
+    }
+
+    hexDisplayEl.appendChild(rowEl);
+  }
+}
+
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
 if (params.hasOwnProperty('debug')) {
   fullscreenEl.classList.add('debug');
 }
+
+const DEFAULT_HEX_GRID_WIDTH  = 19;
+const DEFAULT_HEX_GRID_HEIGHT = 8;
+const gridWidth               = params.hasOwnProperty('w') ? params.w : DEFAULT_HEX_GRID_WIDTH;
+const gridHeight              = params.hasOwnProperty('h') ? params.h : DEFAULT_HEX_GRID_HEIGHT;
+generateHexGrid(gridWidth, gridHeight);
 
 // Initialises Material Design Components
 // See: https://github.com/material-components/material-components-web#javascript
